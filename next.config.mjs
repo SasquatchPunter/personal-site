0d95982099e54953b00ae8e7185c42a41b3118f9
SasquatchPunter <jeremyrobertelliott@gmail.com>
2024-06-19 10:55:19 -0600

    build: Set nextjs output to 'export'

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  images: {
+    remotePatterns: [{ hostname: "cdn.sanity.io" }],
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
