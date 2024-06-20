24a587cab6ece01d09e1ee4a374715b5e8836c99
SasquatchPunter <jeremyrobertelliott@gmail.com>
2024-06-20 12:48:45 -0600

    feat: Make Youtube player reponsive

--- src/components/portable/default/types/Youtube.tsx
+++ src/components/portable/default/types/Youtube.tsx
@@ -3,5 +3,14 @@ import dynamic from "next/dynamic";
 const Player = dynamic(import("react-player/youtube"), { ssr: false });
 
 export default function Youtube(props: any) {
-  return <Player url={props.value.url} controls={true} />;
+  return (
+    <div className="w-full h-96">
+      <Player
+        url={props.value.url}
+        controls={true}
+        width="100%"
+        height="100%"
+      />
+    </div>
+  );
 }
