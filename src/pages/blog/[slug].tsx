412e0b7e48bb8bf527cc676307c4163ad4bfed61
SasquatchPunter <jeremyrobertelliott@gmail.com>
2024-06-19 20:52:04 -0600

    feat: Add functionality to blog post page

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,3 +1,31 @@
-export default function BlogPostRoute() {
-  return <h1>Blog Post Route</h1>;
+import { getPostsPaths, getPostBySlug } from "@/sanity/lib/fetch";
+import BlogPost from "@/src/components/blog/BlogPost";
+import { InferGetStaticPropsType } from "next";
+
+export async function getStaticProps({ params }: { params: any }) {
+  const post = await getPostBySlug(params.slug);
+
+  if (!post) return { notFound: true };
+
+  return { props: { post } };
+}
+
+export default function BlogPostRoute({
+  post,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return <BlogPost post={post} />;
+}
+
+export async function getStaticPaths() {
+  const data = await getPostsPaths();
+  const paths = data.map(({ slug }) => ({
+    params: {
+      slug: slug!.current,
+    },
+  }));
+  return {
+    // this circumvents a regression that causes Next to resolve empty path arrays to all dynamic routes
+    paths: paths.length !== 0 ? paths : [{ params: { slug: "" } }],
+    fallback: false,
+  };
 }
