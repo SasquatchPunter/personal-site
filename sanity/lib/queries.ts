4fb9ee8583239a96c0ae4727b332ce594431a33e
SasquatchPunter <jeremyrobertelliott@gmail.com>
2024-06-20 12:22:18 -0600

    feat: Dereference tags and mainImage fields in blogPost query

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,5 +1,13 @@
 import { groq } from "next-sanity";
 
 // Blog
-export const postsPathsQuery = groq`*[ _type == "blogPost" && defined(slug.current) ][] { slug }`;
-export const postFromSlugQuery = groq`*[ _type == "blogPost" && slug.current == $slug ][0]`;
+export const postsPathsQuery = groq`
+*[ _type == "blogPost" && defined(slug.current) ][] { slug }
+`;
+export const postFromSlugQuery = groq`
+*[ _type == "blogPost" && slug.current == $slug ][0] {
+    ...,
+    tags[]->{key},
+    mainImage{asset->}
+}
+`;
