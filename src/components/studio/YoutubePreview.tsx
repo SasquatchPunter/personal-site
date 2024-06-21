c18892f7c7b64695fe96e5b0ac6b593eb1a5f437
SasquatchPunter <jeremyrobertelliott@gmail.com>
2024-06-21 15:06:17 -0600

    feat: memoize youtube preview via dynamic import
    Dynamically importing the player avoids hydration issues on mount

--- src/components/studio/YoutubePreview.tsx
+++ src/components/studio/YoutubePreview.tsx
@@ -2,11 +2,15 @@ import type { PreviewProps } from "sanity";
 
 import { Flex, Text } from "@sanity/ui";
 import dynamic from "next/dynamic";
-
-const YoutubePlayer = dynamic(import("react-player/youtube"), { ssr: false });
+import { useMemo } from "react";
 
 export default function YoutubePreview(props: PreviewProps) {
   const { title: url } = props;
+
+  const YoutubePlayer = useMemo(
+    () => dynamic(import("react-player/youtube"), { ssr: false }),
+    []
+  );
 
   return (
     <Flex padding={3} align="center" justify="center">
