db6043176e7361abf04dd50eda46f98783512ec1
SasquatchPunter <jeremyrobertelliott@gmail.com>
2024-06-21 14:26:38 -0600

    feat: change urlForImage to output more generalized strings

--- sanity/lib/image.ts
+++ sanity/lib/image.ts
@@ -1,6 +1,6 @@
-import createImageUrlBuilder from "@sanity/image-url";
 import type { Image } from "sanity";
 
+import createImageUrlBuilder from "@sanity/image-url";
 import { dataset, projectId } from "../env";
 
 const imageBuilder = createImageUrlBuilder({
@@ -9,5 +9,5 @@ const imageBuilder = createImageUrlBuilder({
 });
 
 export const urlForImage = (source: Image) => {
-  return imageBuilder?.image(source).auto("format").fit("max").url();
+  return imageBuilder?.image(source).auto("format").url();
 };
