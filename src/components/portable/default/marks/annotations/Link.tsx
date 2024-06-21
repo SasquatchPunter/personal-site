40a4c592c61e75e7edf53c7ed1f17f5630fedf9b
SasquatchPunter <jeremyrobertelliott@gmail.com>
2024-06-21 14:56:02 -0600

    feat: set anchor target to open new tabs

--- src/components/portable/default/marks/annotations/Link.tsx
+++ src/components/portable/default/marks/annotations/Link.tsx
@@ -1,6 +1,6 @@
 export default function Link(props: any) {
   return (
-    <a href={props.value.href}>
+    <a target="_blank" href={props.value.href}>
       <strong className="hover:underline text-rose-800">
         {props.children}
       </strong>
