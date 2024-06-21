27d6f7265a8e010461eb87f35f9279f19fc17116
SasquatchPunter <jeremyrobertelliott@gmail.com>
2024-06-21 15:03:42 -0600

    feat: add copy button to codeblock

--- src/components/portable/default/types/CodeBlock.tsx
+++ src/components/portable/default/types/CodeBlock.tsx
@@ -1,19 +1,24 @@
 import type { PortableTextTypeComponentProps } from "@portabletext/react";
+import { useCallback } from "react";
 import { Light } from "react-syntax-highlighter";
 import { gruvboxDark } from "react-syntax-highlighter/dist/cjs/styles/hljs";
 
 export default function CodeBlock(props: PortableTextTypeComponentProps<any>) {
   const { code, language, filename } = props.value;
 
+  const onClick = useCallback(() => {
+    navigator.clipboard.writeText(code);
+  }, [code]);
+
   return (
     <div className="rounded-xl overflow-hidden bg-stone-200">
-      <div className="flex justify-between text-sm p-2">
+      {/* <div className="flex justify-between text-sm p-2">
         <span>{filename}</span>
-      </div>
+      </div> */}
       <Light language={language} style={gruvboxDark} showLineNumbers>
         {code}
       </Light>
-      <span className="ml-auto">{language}</span>
+      {/* <span className="">{language}</span> */}
     </div>
   );
 }
