ce7af752f101fe08a59ad755137b4f6b02a374a7
SasquatchPunter <jeremyrobertelliott@gmail.com>
2024-06-21 14:28:25 -0600

    feat: remove mainImage dereferencing from postFromSlug query

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -7,7 +7,6 @@ export const postsPathsQuery = groq`
 export const postFromSlugQuery = groq`
 *[ _type == "blogPost" && slug.current == $slug ][0] {
     ...,
-    tags[]->{key},
-    mainImage{asset->}
+    'tags': tags[]->{key},
 }
 `;
