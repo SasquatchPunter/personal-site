838a8fb0ec5b2d3cc2da2e12e377b81c84f0d455
SasquatchPunter <jeremyrobertelliott@gmail.com>
2024-06-26 19:07:34 -0600

    feat: Add screen heights to tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,16 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      height: {
+        "screen-1/4": "25vh",
+        "screen-2/4": "50vh",
+        "screen-1/2": "50vh",
+        "screen-3/4": "75vh",
+        "screen-1/5": "20vh",
+        "screen-2/5": "40vh",
+        "screen-3/5": "60vh",
+        "screen-4/5": "80vh",
+      },
     },
   },
   plugins: [],
