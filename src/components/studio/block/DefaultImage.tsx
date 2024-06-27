ba6df29c5cca759829c04b287c0dcc1565eafd01
SasquatchPunter <jeremyrobertelliott@gmail.com>
2024-06-26 19:10:49 -0600

    style: rename components/studio dirs

---
+++ src/components/studio/block/DefaultImage.tsx
@@ -0,0 +1,16 @@
+import { PreviewProps } from "sanity";
+import { Stack, Text, Card } from "@sanity/ui";
+
+export default function DefaultImage(
+  props: PreviewProps & { caption?: string }
+) {
+  const { caption } = props;
+  return (
+    <Stack>
+      {props.renderDefault(props)}
+      <Card padding={2}>
+        <Text size={1}>{caption}</Text>
+      </Card>
+    </Stack>
+  );
+}
