ba05a7399ec6e4e27db45c2813024d4c051c97d1
SasquatchPunter <jeremyrobertelliott@gmail.com>
2024-07-25 15:59:29 -0400

    feat: Add useBlogFilter hook and tests

---
+++ sanity/hooks/__tests__/useBlogFilter.ts
@@ -0,0 +1,177 @@
+/**
+ * @jest-environment jsdom
+ */
+import { act, renderHook } from "@testing-library/react";
+import useBlogFilter from "../useBlogFilter";
+import { BlogPostFilter } from "@/sanity/utils/blog";
+import { MinPostsQueryResult } from "@/sanity/types";
+
+describe("useBlogFilter()", () => {
+  let hook = renderHook(() => useBlogFilter([]));
+  const posts = () => hook.result.current[0];
+  const actions = () => hook.result.current[1];
+  const filter = () => hook.result.current[2];
+
+  test("Should initialize with correct default.", () => {
+    const { result } = renderHook(() => useBlogFilter([]));
+    const filterState = result.current[2];
+    expect(filterState).toEqual({});
+  });
+
+  test("Should initialize with initial filter object.", () => {
+    const initialFilter: BlogPostFilter = {
+      includeTags: [],
+      excludeTags: ["a", "b"],
+      createdAfter: "2020",
+      createdBefore: "2024",
+      updatedAfter: "2021",
+      updatedBefore: "2025",
+    };
+    const { result } = renderHook(() => useBlogFilter([], initialFilter));
+    const filterState = result.current[2];
+    expect(filterState).toEqual(initialFilter);
+  });
+
+  describe("Can call filter actions.", () => {
+    describe.each(["includeTags", "excludeTags"] as (
+      | "includeTags"
+      | "excludeTags"
+    )[])("actions.%s", (f) => {
+      beforeEach(() => {
+        hook = renderHook(() => useBlogFilter([]));
+      });
+
+      test("add", () => {
+        act(() => {
+          actions()[f].add("a", "c", "b");
+        });
+        expect(filter()[f]).toEqual(["a", "b", "c"]);
+      });
+
+      test("remove", () => {
+        act(() => {
+          actions()[f].add("a");
+          actions()[f].remove("a");
+        });
+        expect(filter()[f]).toEqual([]);
+      });
+
+      test("unset", () => {
+        act(() => {
+          actions()[f].add("a");
+          actions()[f].unset();
+        });
+        expect(filter()[f]).toBe(undefined);
+      });
+    });
+
+    describe.each([
+      "createdBefore",
+      "createdAfter",
+      "updatedBefore",
+      "updatedAfter",
+    ] as (
+      | "createdBefore"
+      | "createdAfter"
+      | "updatedBefore"
+      | "updatedAfter"
+    )[])("actions.%s", (f) => {
+      beforeEach(() => {
+        hook = renderHook(() => useBlogFilter([]));
+      });
+
+      test("set", () => {
+        act(() => {
+          actions()[f].set("2020");
+        });
+        expect(filter()[f]).toBe("2020");
+      });
+
+      test("unset", () => {
+        act(() => {
+          actions()[f].set("2020");
+          actions()[f].unset();
+        });
+        expect(filter()[f]).toBe(undefined);
+      });
+    });
+  });
+
+  describe("Filters posts", () => {
+    const template: MinPostsQueryResult[0] = {
+      _id: "",
+      _createdAt: "",
+      _updatedAt: "",
+      title: "",
+      slug: "",
+      tags: null,
+    };
+
+    const mockPosts: MinPostsQueryResult = [
+      {
+        ...template,
+        _id: "1",
+        _createdAt: "2020-01-01",
+        _updatedAt: "2020-02-01",
+        tags: ["a", "b", "c"],
+      },
+      {
+        ...template,
+        _id: "2",
+        _createdAt: "2019-01-01",
+        _updatedAt: "2021-01-01",
+        tags: ["d"],
+      },
+      {
+        ...template,
+        _id: "3",
+        _createdAt: "2022-01-01",
+        _updatedAt: "2024-06-01",
+      },
+      {
+        ...template,
+        _id: "4",
+        _createdAt: "2020-06-01",
+        _updatedAt: "2022-06-01",
+        tags: ["a", "b"],
+      },
+    ];
+
+    test.each(["includeTags", "excludeTags"] as Array<
+      "includeTags" | "excludeTags"
+    >)("Filters by `filter.%s`", (f) => {
+      hook = renderHook(() =>
+        useBlogFilter([...mockPosts], { [f]: ["a", "d"] })
+      );
+
+      if (f === "includeTags") {
+        expect(posts()).toEqual([mockPosts[0], mockPosts[1], mockPosts[3]]);
+      } else if (f === "excludeTags") {
+        expect(posts()).toEqual([mockPosts[2]]);
+      }
+    });
+
+    test.each([
+      "createdBefore",
+      "createdAfter",
+      "updatedBefore",
+      "updatedAfter",
+    ] as Array<
+      "createdBefore" | "createdAfter" | "updatedBefore" | "updatedAfter"
+    >)("Filters by `filter.%s`", (f) => {
+      hook = renderHook(() =>
+        useBlogFilter([...mockPosts], { [f]: "2021-06-01" })
+      );
+
+      if (f === "createdBefore") {
+        expect(posts()).toEqual([mockPosts[0], mockPosts[1], mockPosts[3]]);
+      } else if (f === "createdAfter") {
+        expect(posts()).toEqual([mockPosts[2]]);
+      } else if (f === "updatedBefore") {
+        expect(posts()).toEqual([mockPosts[0], mockPosts[1]]);
+      } else if (f === "updatedAfter") {
+        expect(posts()).toEqual([mockPosts[2], mockPosts[3]]);
+      }
+    });
+  });
+});
---
+++ sanity/hooks/useBlogFilter.ts
@@ -0,0 +1,208 @@
+import type { BlogPostFilter } from "@/sanity/utils/blog";
+import type { MinPostsQueryResult } from "../types";
+import type { Dispatch } from "react";
+
+import { useMemo, useReducer } from "react";
+import { filterPosts } from "@/sanity/utils/blog";
+
+enum ActionType {
+  ADD_INCLUDED_TAGS,
+  ADD_EXCLUDED_TAGS,
+  REMOVE_INCLUDED_TAGS,
+  REMOVE_EXCLUDED_TAGS,
+  UNSET_INCLUDED_TAGS,
+  UNSET_EXCLUDED_TAGS,
+  SET_CREATED_BEFORE,
+  SET_CREATED_AFTER,
+  SET_UPDATED_BEFORE,
+  SET_UPDATED_AFTER,
+  UNSET_CREATED_BEFORE,
+  UNSET_CREATED_AFTER,
+  UNSET_UPDATED_BEFORE,
+  UNSET_UPDATED_AFTER,
+}
+
+type Action =
+  | {
+      type:
+        | ActionType.ADD_INCLUDED_TAGS
+        | ActionType.ADD_EXCLUDED_TAGS
+        | ActionType.REMOVE_INCLUDED_TAGS
+        | ActionType.REMOVE_EXCLUDED_TAGS;
+      payload: string[];
+    }
+  | {
+      type: ActionType.UNSET_INCLUDED_TAGS | ActionType.UNSET_EXCLUDED_TAGS;
+      payload?: any;
+    }
+  | {
+      type:
+        | ActionType.SET_CREATED_BEFORE
+        | ActionType.SET_CREATED_AFTER
+        | ActionType.SET_UPDATED_BEFORE
+        | ActionType.SET_UPDATED_AFTER;
+      payload: string;
+    }
+  | {
+      type:
+        | ActionType.UNSET_CREATED_BEFORE
+        | ActionType.UNSET_CREATED_AFTER
+        | ActionType.UNSET_UPDATED_BEFORE
+        | ActionType.UNSET_UPDATED_AFTER;
+      payload?: any;
+    };
+
+function filterDispatchers(dispatch: Dispatch<Action>) {
+  return {
+    includeTags: {
+      add(...tags: string[]) {
+        dispatch({ type: ActionType.ADD_INCLUDED_TAGS, payload: tags });
+      },
+      remove(...tags: string[]) {
+        dispatch({ type: ActionType.REMOVE_INCLUDED_TAGS, payload: tags });
+      },
+      unset() {
+        dispatch({ type: ActionType.UNSET_INCLUDED_TAGS });
+      },
+    },
+    excludeTags: {
+      add(...tags: string[]) {
+        dispatch({ type: ActionType.ADD_EXCLUDED_TAGS, payload: tags });
+      },
+      remove(...tags: string[]) {
+        dispatch({ type: ActionType.REMOVE_EXCLUDED_TAGS, payload: tags });
+      },
+      unset() {
+        dispatch({ type: ActionType.UNSET_EXCLUDED_TAGS });
+      },
+    },
+    createdBefore: {
+      set(date: string) {
+        dispatch({ type: ActionType.SET_CREATED_BEFORE, payload: date });
+      },
+      unset() {
+        dispatch({ type: ActionType.UNSET_CREATED_BEFORE });
+      },
+    },
+    createdAfter: {
+      set(date: string) {
+        dispatch({ type: ActionType.SET_CREATED_AFTER, payload: date });
+      },
+      unset() {
+        dispatch({ type: ActionType.UNSET_CREATED_AFTER });
+      },
+    },
+    updatedBefore: {
+      set(date: string) {
+        dispatch({ type: ActionType.SET_UPDATED_BEFORE, payload: date });
+      },
+      unset() {
+        dispatch({ type: ActionType.UNSET_UPDATED_BEFORE });
+      },
+    },
+    updatedAfter: {
+      set(date: string) {
+        dispatch({ type: ActionType.SET_UPDATED_AFTER, payload: date });
+      },
+      unset() {
+        dispatch({ type: ActionType.UNSET_UPDATED_AFTER });
+      },
+    },
+  };
+}
+
+function filterReducer(filter: BlogPostFilter, action: Action): BlogPostFilter {
+  const { type, payload } = action;
+
+  let includeTags: string[] = filter.includeTags ? [...filter.includeTags] : [];
+  let excludeTags: string[] = filter.excludeTags ? [...filter.excludeTags] : [];
+
+  switch (type) {
+    case ActionType.ADD_INCLUDED_TAGS:
+      for (const tag of payload) {
+        if (!includeTags.includes(tag)) {
+          includeTags.push(tag);
+        }
+      }
+      return { ...filter, includeTags: includeTags.sort() };
+
+    case ActionType.ADD_EXCLUDED_TAGS:
+      for (const tag of payload) {
+        if (!excludeTags.includes(tag)) {
+          excludeTags.push(tag);
+        }
+      }
+      return { ...filter, excludeTags: excludeTags.sort() };
+
+    case ActionType.REMOVE_INCLUDED_TAGS:
+      includeTags = includeTags.filter((tag) => !payload.includes(tag));
+      return { ...filter, includeTags };
+
+    case ActionType.REMOVE_EXCLUDED_TAGS:
+      excludeTags = excludeTags.filter((tag) => !payload.includes(tag));
+      return { ...filter, excludeTags };
+
+    case ActionType.UNSET_INCLUDED_TAGS:
+      delete filter.includeTags;
+      return { ...filter };
+
+    case ActionType.UNSET_EXCLUDED_TAGS:
+      delete filter.excludeTags;
+      return { ...filter };
+
+    case ActionType.SET_CREATED_BEFORE:
+      return { ...filter, createdBefore: payload };
+
+    case ActionType.SET_CREATED_AFTER:
+      return { ...filter, createdAfter: payload };
+
+    case ActionType.SET_UPDATED_BEFORE:
+      return { ...filter, updatedBefore: payload };
+
+    case ActionType.SET_UPDATED_AFTER:
+      return { ...filter, updatedAfter: payload };
+
+    case ActionType.UNSET_CREATED_BEFORE:
+      delete filter.createdBefore;
+      return { ...filter };
+
+    case ActionType.UNSET_CREATED_AFTER:
+      delete filter.createdAfter;
+      return { ...filter };
+
+    case ActionType.UNSET_UPDATED_BEFORE:
+      delete filter.updatedBefore;
+      return { ...filter };
+
+    case ActionType.UNSET_UPDATED_AFTER:
+      delete filter.updatedAfter;
+      return { ...filter };
+
+    default:
+      throw new Error(ACTION_TYPE_UNSUPPORTED_ERROR);
+  }
+}
+
+export const ACTION_TYPE_UNSUPPORTED_ERROR =
+  "An unsupported filter action was used!";
+
+/**
+ *
+ * @param posts Pass in original unfiltered posts
+ * @param initialFilter Optional filter object to initialize to
+ * @returns An array containing the filtered posts, filter actions, and the filter state object.
+ */
+export default function useBlogFilter(
+  posts: MinPostsQueryResult,
+  initialFilter: BlogPostFilter = {}
+): [MinPostsQueryResult, ReturnType<typeof filterDispatchers>, BlogPostFilter] {
+  const [filterState, dispatch] = useReducer(filterReducer, initialFilter);
+
+  const filteredPosts = useMemo(() => {
+    return filterPosts(posts, filterState);
+  }, [posts, filterState]);
+
+  const filterActions = useMemo(() => filterDispatchers(dispatch), []);
+
+  return [filteredPosts, filterActions, filterState];
+}
