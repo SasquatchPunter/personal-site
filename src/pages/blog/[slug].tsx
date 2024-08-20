4f3bfa966db81f08fc367dac6bb2f9fed2d04342
SasquatchPunter <jeremyrobertelliott@gmail.com>
2024-08-20 04:05:52 -0400

    feat: Update /blog/[slug] to use site settings

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,16 +1,23 @@
 import type { InferGetStaticPropsType } from "next";
 import type { Anchors } from "@/sanity/utils/blog";
 
-import { getPostsPaths, getPostBySlug } from "@/sanity/lib/fetch";
+import {
+  getPostsPaths,
+  getPostBySlug,
+  getSiteSettings,
+} from "@/sanity/lib/fetch";
 import { tocTreeFromAnchors } from "@/sanity/utils/blog";
 
 import BlogPost from "@/src/components/blog/BlogPost";
 import PageLayout from "@/src/components/layout/shared/PageLayout";
+import Footer from "@/src/components/Footer";
 
 export async function getStaticProps({ params }: { params: any }) {
   const post = await getPostBySlug(params.slug);
 
   if (!post) return { notFound: true };
+
+  const siteSettings = await getSiteSettings();
 
   const toc = tocTreeFromAnchors(post.anchors as Anchors) || null;
 
@@ -20,6 +27,7 @@ export async function getStaticProps({ params }: { params: any }) {
         ...post,
         toc,
       },
+      siteSettings,
     },
   };
 }
@@ -40,10 +48,17 @@ export async function getStaticPaths() {
 
 export type BlogPostPageProps = InferGetStaticPropsType<typeof getStaticProps>;
 
-export default function BlogPostPage({ post }: BlogPostPageProps) {
+export default function BlogPostPage({
+  post,
+  siteSettings,
+}: BlogPostPageProps) {
   return (
-    <PageLayout title={post.title}>
+    <PageLayout
+      siteTitle={siteSettings?.siteTitle || undefined}
+      title={post.title}
+    >
       <BlogPost post={post} />
+      <Footer socialLinks={siteSettings?.socialLinks || null} />
     </PageLayout>
   );
 }
