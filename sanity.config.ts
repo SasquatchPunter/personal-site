6b0c967be4f8b44adfdb5ac32c3aaa26161d95f3
SasquatchPunter <jeremyrobertelliott@gmail.com>
2024-08-20 04:00:41 -0400

    refactor: update sanity.config

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,17 +2,16 @@ import { visionTool } from "@sanity/vision";
 import { colorInput } from "@sanity/color-input";
 import { codeInput } from "@sanity/code-input";
 import { defineConfig } from "sanity";
-// import { defineConfig } from "@sanity-typed/types";
 import { structureTool } from "sanity/structure";
 
-import { apiVersion, dataset, projectId } from "./sanity/env";
-import { schema } from "./sanity/schema";
-import { structure } from "./sanity/structure";
+import { apiVersion, dataset, projectId } from "@/sanity/env";
+import { schema } from "@/sanity/schema";
+import structure from "@/sanity/structure";
 
 import Icon from "@/src/assets/icons/favicon.svg";
 
 export default defineConfig({
-  title: "JeremyElliott.dev",
+  title: "jelliott.dev",
   icon: Icon,
   basePath: "/admin",
   projectId,
