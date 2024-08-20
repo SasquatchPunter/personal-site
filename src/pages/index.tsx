d4f7cd82c2c73615b37f9c23e12cd928b135ef83
SasquatchPunter <jeremyrobertelliott@gmail.com>
2024-08-20 04:06:39 -0400

    feat: Update / route with Footer and site settings

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,23 @@
-import PageLayout from "@/src/components/layout/shared/PageLayout";
+import type { InferGetStaticPropsType } from "next";
 
-export default function HomePage() {
+import { getSiteSettings } from "@/sanity/lib/fetch";
+
+import PageLayout from "@/src/components/layout/shared/PageLayout";
+import Footer from "@/src/components/Footer";
+
+export async function getStaticProps() {
+  const siteSettings = await getSiteSettings();
+
+  return { props: { siteSettings } };
+}
+
+export type HomePageProps = InferGetStaticPropsType<typeof getStaticProps>;
+
+export default function HomePage({ siteSettings }: HomePageProps) {
   return (
-    <PageLayout title="Home">
+    <PageLayout siteTitle={siteSettings?.siteTitle || undefined} title="Home">
       <h1>Home Page</h1>
+      <Footer socialLinks={siteSettings?.socialLinks || null} />
     </PageLayout>
   );
 }
