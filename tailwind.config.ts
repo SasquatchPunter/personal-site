5a1d7746e59ca80dad4ebdfc37189184ca01651a
SasquatchPunter <jeremyrobertelliott@gmail.com>
2024-08-20 03:54:34 -0400

    feat: Add tailwind variants and config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,13 @@
 import type { Config } from "tailwindcss";
+import type { PluginCreator } from "tailwindcss/types/config";
+
+const addVariants: PluginCreator = ({ addVariant }) => {
+  addVariant("svg", "& > svg");
+  addVariant("svg_*", "& > svg *");
+
+  addVariant("path", "& > path");
+  addVariant("path_*", "& > path *");
+};
 
 const config: Config = {
   content: [
@@ -7,15 +16,29 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        base: {
+          1: "#adffb0",
+        },
+      },
+      dropShadow: {
+        "lg-t": "0 -0.5rem 1rem var(--tw-shadow-color)",
+        "lg-b": "0 0.5rem 1rem var(--tw-shadow-color)",
+        "lg-l": "-0.5rem 0 1rem var(--tw-shadow-color)",
+        "lg-r": "0.5rem 0 1rem var(--tw-shadow-color)",
+        "lg-c": "0 0 1rem var(--tw-shadow-color)",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       height: {
+        "screen-1/2": "50vh",
+        "screen-1/3": "33vh",
+        "screen-2/3": "66vh",
         "screen-1/4": "25vh",
         "screen-2/4": "50vh",
-        "screen-1/2": "50vh",
         "screen-3/4": "75vh",
         "screen-1/5": "20vh",
         "screen-2/5": "40vh",
@@ -24,6 +47,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [addVariants],
 };
 export default config;
