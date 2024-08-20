011ee1f8d5744c5bf982d7af813077428ca9fa18
SasquatchPunter <jeremyrobertelliott@gmail.com>
2024-08-20 04:05:14 -0400

    feat: Add blog list to /blog page

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,9 +1,29 @@
-import PageLayout from "@/src/components/layout/shared/PageLayout";
+import type { InferGetStaticPropsType } from "next";
 
-export default function BlogPage() {
+import { getSiteSettings, getMinPosts } from "@/sanity/lib/fetch";
+import { extractTags } from "@/sanity/utils/blog";
+
+import PageLayout from "@/src/components/layout/shared/PageLayout";
+import BlogList from "@/src/components/blog/BlogList";
+import Footer from "@/src/components/Footer";
+
+export async function getStaticProps() {
+  const siteSettings = await getSiteSettings();
+  const posts = await getMinPosts();
+
+  const tags = extractTags(posts);
+
+  return { props: { posts, tags, siteSettings } };
+}
+
+export type BlogPageProps = InferGetStaticPropsType<typeof getStaticProps>;
+
+export default function BlogPage({ posts, tags, siteSettings }: BlogPageProps) {
   return (
-    <PageLayout title="Blog">
+    <PageLayout siteTitle={siteSettings?.siteTitle || undefined} title="Blog">
       <h1>Main Blog Route</h1>
+      <BlogList posts={posts} tags={tags} />
+      <Footer socialLinks={siteSettings?.socialLinks || null} />
     </PageLayout>
   );
 }
