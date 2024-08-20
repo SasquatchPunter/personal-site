991c647e245dd9105f4d10e4d11ade5c079e4ac4
SasquatchPunter <jeremyrobertelliott@gmail.com>
2024-08-20 03:53:12 -0400

    feat: Update types utils

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,2 +1,10 @@
-/** Creates a union of types of properties selected from another type. */
+/** Union of types from a selected member. */
 export type PickUnion<T, K extends keyof T> = T[K];
+
+/** Awaited return type. */
+export type AwaitedReturn<T extends (...args: any[]) => any> = Awaited<
+  ReturnType<T>
+>;
+
+/** NonNullable type alias */
+export type NN<T> = NonNullable<T>;
