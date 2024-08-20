21285f004f53c492c633549fd6541719d76274c3
SasquatchPunter <jeremyrobertelliott@gmail.com>
2024-08-20 14:17:56 -0400

    refactor: Update Social Links to use a title attribute

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -5,6 +5,24 @@ import LinkedIn from "@/src/assets/icons/socialLinks/linkedin.svg";
 import Github from "@/src/assets/icons/socialLinks/github.svg";
 import Default from "@/src/assets/icons/socialLinks/default.svg";
 
+interface SocialLinkAnchorProps {
+  children: React.ReactNode;
+  href: string;
+  title?: string;
+}
+function SocialLinkAnchor({ children, href, title }: SocialLinkAnchorProps) {
+  return (
+    <a
+      href={href}
+      target="_blank"
+      className="*:w-6 *:h-6 opacity-70 hover:opacity-100 duration-300 *:shadow-transparent *:hover:translate-y-0.5 *:hover:shadow-base-1 *:drop-shadow-lg-c *:hover:drop-shadow-lg-b *:duration-300"
+      title={title}
+    >
+      {children}
+    </a>
+  );
+}
+
 interface SocialLinkProps {
   type: string;
   address: string;
@@ -12,28 +30,32 @@ interface SocialLinkProps {
 function SocialLink({ type, address }: SocialLinkProps) {
   const href = ["email"].includes(type) ? `mailto:${address}` : address;
 
-  const Icon = () => {
-    switch (type) {
-      case "email":
-        return <Email className="*:stroke-base-1" />;
-      case "linkedin":
-        return <LinkedIn className="*:fill-base-1" />;
-      case "github":
-        return <Github className="*:fill-base-1" />;
-      default:
-        return <Default className="*:stroke-base-1" />;
-    }
-  };
-
-  return (
-    <a
-      href={href}
-      target="_blank"
-      className="*:hover:translate-y-0.5 *:w-6 *:h-6 *:shadow-transparent *:hover:shadow-base-1 *:drop-shadow-lg-t *:hover:drop-shadow-lg-b *:duration-300"
-    >
-      <Icon />
-    </a>
-  );
+  switch (type) {
+    case "email":
+      return (
+        <SocialLinkAnchor href={href} title="Email">
+          <Email className="*:stroke-base-1" />
+        </SocialLinkAnchor>
+      );
+    case "linkedin":
+      return (
+        <SocialLinkAnchor href={href} title="LinkedIn">
+          <LinkedIn className="*:fill-base-1" />
+        </SocialLinkAnchor>
+      );
+    case "github":
+      return (
+        <SocialLinkAnchor href={href} title="Github">
+          <Github className="*:fill-base-1" />
+        </SocialLinkAnchor>
+      );
+    default:
+      return (
+        <SocialLinkAnchor href={href}>
+          <Default className="*:stroke-base-1" />
+        </SocialLinkAnchor>
+      );
+  }
 }
 
 interface Props {
