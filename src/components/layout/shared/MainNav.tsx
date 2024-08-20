3cf6d1ece7c28aa535a8fe52a67c25fe256c4aeb
SasquatchPunter <jeremyrobertelliott@gmail.com>
2024-08-20 14:18:24 -0400

    feat: Update main nav style

--- src/components/layout/shared/MainNav.tsx
+++ src/components/layout/shared/MainNav.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function MainNav() {
   return (
-    <nav className="sticky top-0 flex flex-row gap-4 items-center p-2">
+    <nav className="sticky top-0 flex flex-row gap-4 items-center p-2 backdrop-blur-sm text-base-1 font-black">
       <Link href="/">
         <Icon className="h-12 w-auto aspect-square hover:scale-110 duration-100" />
       </Link>
