403944a9387bd29ea5e5a5737e730ef21e6380a0
SasquatchPunter <jeremyrobertelliott@gmail.com>
2024-08-29 00:45:50 -0400

    feat: Add Cursor to PageLayout

--- src/components/layout/shared/PageLayout.tsx
+++ src/components/layout/shared/PageLayout.tsx
@@ -2,18 +2,21 @@ import type { ReactNode } from "react";
 
 import Head from "next/head";
 import MainNav from "@/src/components/layout/shared/MainNav";
+import Cursor from "@/src/components/Cursor";
 
 interface Props {
   siteTitle?: string;
   title?: string;
   children?: ReactNode;
   hasNav?: boolean;
+  hasCustomCursor?: boolean;
 }
 export default function PageLayout({
   siteTitle = "jelliott.dev",
   title = "Page",
   children,
   hasNav = true,
+  hasCustomCursor = true,
 }: Props) {
   return (
     <>
@@ -29,6 +32,7 @@ export default function PageLayout({
         />
         <link rel="manifest" href="/manifest.json" />
       </Head>
+      {hasCustomCursor ? <Cursor /> : null}
       {hasNav ? <MainNav /> : null}
       {children}
     </>
