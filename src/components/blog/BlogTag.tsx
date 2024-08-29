2f1369ac1a8a85b732eaee95580e93c5dfe0158b
SasquatchPunter <jeremyrobertelliott@gmail.com>
2024-08-29 01:45:31 -0400

    feat: Adopt new border width class in BlogTag.tsx

--- src/components/blog/BlogTag.tsx
+++ src/components/blog/BlogTag.tsx
@@ -3,7 +3,7 @@ interface Props {
 }
 export default function BlogTag({ tag }: Props) {
   return (
-    <span className="inline-block text-green-50 bg-green-950 bg-opacity-100 rounded-md border-2 border-green-200 border-opacity-50 px-2 py-1 before:content-['#']">
+    <span className="inline-block text-green-50 bg-green-950 bg-opacity-100 rounded-md border-2px border-green-200 border-opacity-50 px-2 py-1 before:content-['#']">
       {tag}
     </span>
   );
