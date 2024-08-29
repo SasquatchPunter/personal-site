ef690d6c02e39b099aa8a58eb74d8169074888a9
SasquatchPunter <jeremyrobertelliott@gmail.com>
2024-08-29 01:48:38 -0400

    feat: Add CursorProvider to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,12 @@ import "@/src/styles/globals.css";
 
 import type { AppProps } from "next/app";
 
+import CursorProvider from "@/src/contexts/CursorContext";
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <CursorProvider>
+      <Component {...pageProps} />
+    </CursorProvider>
+  );
 }
