063c45feb7b6ec72dc089134c251ed38349efc8c
SasquatchPunter <jeremyrobertelliott@gmail.com>
2024-08-30 11:22:00 -0400

    feat: Wrap blog post YT component with CursorHover

--- src/components/portableText/default/types/Youtube.tsx
+++ src/components/portableText/default/types/Youtube.tsx
@@ -1,6 +1,8 @@
 import dynamic from "next/dynamic";
 import { useMemo } from "react";
 
+import CursorHover from "@/src/components/CursorHover";
+
 export default function Youtube(props: any) {
   const Player = useMemo(
     () => dynamic(import("react-player/youtube"), { ssr: false }),
@@ -8,12 +10,18 @@ export default function Youtube(props: any) {
   );
   return (
     <div className="w-full h-96">
-      <Player
-        url={props.value.url}
-        controls={true}
-        width="100%"
-        height="100%"
-      />
+      <CursorHover
+        enterEffect="hide"
+        leaveEffect="unhide"
+        className="size-full"
+      >
+        <Player
+          url={props.value.url}
+          controls={true}
+          width="100%"
+          height="100%"
+        />
+      </CursorHover>
     </div>
   );
 }
