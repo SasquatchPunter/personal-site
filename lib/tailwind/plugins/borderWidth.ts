207c78d1afd85281d9563251497c5019a78299b0
SasquatchPunter <jeremyrobertelliott@gmail.com>
2024-08-31 01:21:10 -0400

    refactor: clean up borderWidth tailwind plugin

--- lib/tailwind/plugins/borderWidth.ts
+++ lib/tailwind/plugins/borderWidth.ts
@@ -6,14 +6,6 @@ export default function borderWidth({ theme }: PluginUtils) {
 
   for (const [key, value] of Object.entries<string>(spacing)) {
     borderWidth[key] = value;
-
-    borderWidth[`x-${key}`] = value;
-    borderWidth[`y-${key}`] = value;
-
-    borderWidth[`t-${key}`] = value;
-    borderWidth[`b-${key}`] = value;
-    borderWidth[`l-${key}`] = value;
-    borderWidth[`r-${key}`] = value;
   }
 
   return borderWidth;
