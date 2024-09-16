a5888d9d60d0df6b4b3202a13121f9bd9f478a2e
SasquatchPunter <jeremyrobertelliott@gmail.com>
2024-09-16 14:25:39 -0400

    feat: Style nav. Add Anton font to nav class

--- src/components/layout/shared/MainNav.tsx
+++ src/components/layout/shared/MainNav.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 
 export default function MainNav() {
   return (
-    <nav className="sticky top-0 flex flex-row gap-4 items-center p-2 backdrop-blur-sm text-base-1 font-black">
+    <nav className="font-anton sticky top-0 flex flex-row gap-4 items-center text-xl p-4 backdrop-blur-sm text-base-1 font-black">
       <Link href="/">
-        <Icon className="h-12 w-auto aspect-square hover:scale-110 duration-100" />
+        <Icon className="h-16 w-auto aspect-square hover:scale-110 duration-100" />
       </Link>
       <Link href="/blog" className="h-fit">
         Blog
