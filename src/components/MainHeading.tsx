e08dcbb8a5bf6ca1e183e6259a8e8e3a1e2b6653
SasquatchPunter <jeremyrobertelliott@gmail.com>
2024-09-16 14:27:26 -0400

    feat: Style main heading font

--- src/components/MainHeading.tsx
+++ src/components/MainHeading.tsx
@@ -6,8 +6,8 @@ interface Props {
 }
 export default function MainHeading({ children }: Props) {
   return (
-    <h1 className="font-black size-fit mx-auto p-4 text-6xl text-center">
-      <CursorHover enterEffect="activate" leaveEffect="deactivate" span>
+    <h1 className="font-anton mx-auto p-4 text-6xl text-center">
+      <CursorHover enterEffect="activate" leaveEffect="deactivate">
         <SplitText className="main-heading">{children}</SplitText>
       </CursorHover>
     </h1>
