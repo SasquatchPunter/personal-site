e40b539c3e2f2ff87c7bc25224099031b46d7d49
SasquatchPunter <jeremyrobertelliott@gmail.com>
2024-09-16 14:24:58 -0400

    feat: Add Anton font to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,8 @@ import { getSiteSettings } from "@/sanity/lib/fetch";
 
 import PageLayout from "@/src/components/layout/shared/PageLayout";
 import Footer from "@/src/components/layout/shared/Footer";
-import MainHeading from "../components/MainHeading";
+import MainHeading from "@/src/components/MainHeading";
+import AntonFont from "@/src/components/layout/fonts/AntonFont";
 
 export async function getStaticProps() {
   const siteSettings = await getSiteSettings();
@@ -16,9 +17,11 @@ export type HomePageProps = InferGetStaticPropsType<typeof getStaticProps>;
 
 export default function HomePage({ siteSettings }: HomePageProps) {
   return (
-    <PageLayout title={siteSettings?.siteTitle} subtitle="Home">
-      <MainHeading>Home</MainHeading>
-      <Footer socialLinks={siteSettings?.socialLinks || null} />
-    </PageLayout>
+    <AntonFont>
+      <PageLayout title={siteSettings?.siteTitle} subtitle="Home">
+        <MainHeading>Home</MainHeading>
+        <Footer socialLinks={siteSettings?.socialLinks || null} />
+      </PageLayout>
+    </AntonFont>
   );
 }
