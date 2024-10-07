c496ff7b6ca8c2d511df483aac4c80cbb57c265f
SasquatchPunter <jeremyrobertelliott@gmail.com>
2024-10-07 13:01:32 -0600

    feat: Add nav menu icon to nav

--- src/components/layout/shared/MainNav.tsx
+++ src/components/layout/shared/MainNav.tsx
@@ -1,15 +1,23 @@
 import Icon from "@/src/assets/icons/favicon.svg";
+import MenuIcon from "@/src/assets/icons/menu.svg";
 import Link from "next/link";
 
 export default function MainNav() {
   return (
-    <nav className="font-anton sticky top-0 flex flex-row gap-4 items-center text-xl p-4 backdrop-blur-sm text-base-1 font-black">
+    <nav className="font-anton sticky top-0 flex flex-row justify-between text-xl p-4 backdrop-blur-sm text-base-1 font-black">
       <Link href="/">
-        <Icon className="h-16 w-auto aspect-square hover:scale-110 duration-100" />
+        <Icon className="h-12 opacity-75 hover:opacity-100 w-auto aspect-square hover:scale-110 duration-200" />
       </Link>
-      <Link href="/blog" className="h-fit">
-        Blog
-      </Link>
+      <details className="relative">
+        <summary className="block opacity-75 hover:opacity-100 cursor-pointer">
+          <MenuIcon className="stroke-base-1 h-12 w-auto" />
+        </summary>
+        <section className="absolute flex flex-col right-0">
+          <Link href="/about">About</Link>
+          <Link href="/blog">Blog</Link>
+          <Link href="/contact">Contact</Link>
+        </section>
+      </details>
     </nav>
   );
 }
