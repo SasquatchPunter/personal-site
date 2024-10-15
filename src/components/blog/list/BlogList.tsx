b14e01beec1ad62a8cca4efe5c20f5b84720ddae
SasquatchPunter <jeremyrobertelliott@gmail.com>
2024-10-15 17:15:27 -0600

    feat: update

--- src/components/blog/list/BlogList.tsx
+++ src/components/blog/list/BlogList.tsx
@@ -22,10 +22,10 @@ export default function BlogList({ posts, tags }: Props) {
   }, [filterQuery, filterActions]);
 
   return (
-    <>
+    <div className="mx-8">
       <BlogListFilter filterState={filterState} filterActions={filterActions} />
       {filterQueryReady ? (
-        <ul className="flex flex-col m-auto w-fit gap-4">
+        <ul className="flex flex-col m-auto w-full my-12 gap-12">
           {filteredPosts.map((post) => (
             <BlogListCard
               post={post}
@@ -36,6 +36,6 @@ export default function BlogList({ posts, tags }: Props) {
           ))}
         </ul>
       ) : null}
-    </>
+    </div>
   );
 }
