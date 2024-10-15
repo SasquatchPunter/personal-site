8a22840742895ec0f60e0bba8c5a94ac8395f96b
SasquatchPunter <jeremyrobertelliott@gmail.com>
2024-10-15 16:00:21 -0600

    feat: add overflow-x-hidden to body classes

--- src/components/layout/shared/Body.tsx
+++ src/components/layout/shared/Body.tsx
@@ -1,6 +1,11 @@
 interface Props {
-  children: React.ReactNode;
+  className?: string;
+  children?: React.ReactNode;
 }
-export default function Body({ children }: Props) {
-  return <body>{children}</body>;
+export default function Body({ children, className }: Props) {
+  return (
+    <body className={["overflow-x-hidden", className].join(" ")}>
+      {children}
+    </body>
+  );
 }
